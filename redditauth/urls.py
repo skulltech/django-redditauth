aeb67f733d3cf05431684d34c9a213072a245cc7
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-24 04:22:39 +0530

    Create urls.py

--- redditauth/urls.py
+++ redditauth/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import authorize
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', )
+    url(r'^authorize/', authorize)
 ]
