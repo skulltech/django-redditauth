30cc798988177e690b419f1bed0bce78e903369d
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-28 21:35:13 +0530

    Complete authenticate method

--- redditauth/models.py
+++ redditauth/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import praw
+import json
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
@@ -25,7 +27,27 @@ class RedditUser(AbstractUser):
 
 
 class RedditBackend:
-    def get_user(self, username):
+    @staticmethod
+    def authenticate(request, username=None, password=None, code=None):
+        try:
+            user = RedditUser.objects.get(username=username)
+        except RedditUser.DoesNotExist:
+            if code:
+                with open('secret.json', 'r') as f:
+                    secret = json.load(f)
+
+                reddit = praw.Reddit(client_id=secret['client_id'], client_secret=secret['client_secret'],
+                                     redirect_uri='http://localhost:8000/callback',
+                                     user_agent='Plan-Reddit by /u/SkullTech101')
+                token = reddit.auth.authorize(code)
+                user = RedditUser(username=reddit.user.me(), token=token)
+                return user
+            else:
+                user = RedditUser(username=username, password=password)
+                return user
+
+    @staticmethod
+    def get_user(username):
         try:
             RedditUser.objects.get(username=username)
         except RedditUser.DoesNotExist:
