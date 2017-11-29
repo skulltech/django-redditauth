1d7e5197685268b1e31df725badcec9bf0df7482
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-30 03:58:43 +0530

    Create setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-redditauth',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='An application for Reddit OAuth2 authentication for your Django project.',
+    long_description=README,
+    url='https://sumit-ghosh.com/django-redditauth',
+    author='Sumit Ghosh',
+    author_email='sumit@sumit-ghosh.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
